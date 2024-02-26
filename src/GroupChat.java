7aff8561352b116a214c961dd3d290f2173ef1ce
Sean B <strawhorse@gmail.com>
2024-02-26 21:25:31 +0000

    Need to create new class for the ReadThread portion

--- src/GroupChat.java
+++ src/GroupChat.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class GroupChat {
@@ -23,6 +22,8 @@ public class GroupChat {
             try{
                 InetAddress group = InetAddress.getByName(args[0]);
 //                The InetAddress class is used to encapsulate both, the numerical IP address and the domain name for that address
+//                Java InetAddress class represents an IP address. The java.net.InetAddress class provides methods to get the IP of any host name
+
                 int port = Integer.parseInt(args[1]);
                 Scanner scan = new Scanner(System.in);
                 System.out.println("Please enter your name: \n");
@@ -31,7 +32,32 @@ public class GroupChat {
 //                This class is used for sending and receiving multicast IP packets.
 //                It extends DatagramSocket class and provides additional functionality for joining groups.
                 MulticastSocket socket = new MulticastSocket(Integer.parseInt(args[1]));
-                
+
+
+//                since deploying
+                socket.setTimeToLive(0);
+//                for localhost only
+
+                socket.joinGroup(group);
+                Thread t = new Thread(new ReadThread(socket, group, port));
+
+//                create a thread for reading messages
+                t.start();
+
+//                Message sent to current group
+                System.out.println("Starting typing your messages here: \n");
+
+                while(true) {
+                    String message;
+                    message = scan.nextLine();
+                    if(message.equalsIgnoreCase(GroupChat.TERMINATE)){
+                        finished = true;
+                        socket.leaveGroup(group);
+                        socket.close();
+                        break;
+                    }
+                    message = name + ": " + message;
+                }
 
 
             }
