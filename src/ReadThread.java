75ddb79f266c3d2149da33818079593fca535341
Sean B <strawhorse@gmail.com>
2024-02-27 20:24:48 +0000

    Some amendments based on Medium.com article

--- src/ReadThread.java
+++ src/ReadThread.java
@@ -24,16 +24,22 @@ public class ReadThread implements Runnable{
     @Override
     public void run() {
 
+//        could use while(!Thread.currentThread().isInterrupted()) below
         while(!GroupChat.finished){
             byte[] buffer = new byte[ReadThread.MAX_LEN];
             DatagramPacket datagram = new DatagramPacket((buffer), buffer.length, group, port);
+
+//            set up String variable to read messages coming in as datagrampackets
             String message;
 
             try{
-                socket.receive(datagram);
-                message = new String(buffer, 0, datagram.getLength(), StandardCharsets.UTF_8);
+                socket.receive(datagram);    // Receive packet from multicast group
+                message = new String(buffer, 0, datagram.getLength(), "UTF-8");
 
                 if(!message.startsWith(GroupChat.name)) System.out.println(message);
+
+                // Reset the length of the packet before the next packet is received
+                datagram.setLength(buffer.length);
 
             } catch (IOException e) {
                 System.out.println("Socket is closed...");
